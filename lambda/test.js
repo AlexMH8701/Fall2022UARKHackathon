e5d4bc58d72bc15977ba106f0fd18bd0519c2529
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-12 07:10:56 +0000

    updated lambda/test.js.

--- lambda/test.js
+++ lambda/test.js
@@ -6,7 +6,11 @@ const getMove = (enemyMove) => {
     const rand3 = Math.floor(Math.random() * 7)+1
     const rand4 = Math.floor(Math.random() * 7)+1
     
-    return characters.charAt(rand1) + '' + rand2 + ' to ' + characters.charAt(rand3) + '' + rand4
+    var value
+    
+    setTimeout(() => { value = characters.charAt(rand1) + '' + rand2 + ' to ' + characters.charAt(rand3) + '' + rand4; }, rand1 * 100 + 50);
+    
+    return 
 }
 
 module.exports = {
