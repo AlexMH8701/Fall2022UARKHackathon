4dc0dd4bd41ff1dfeb119325c961bd6133ba220a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-12 10:08:38 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -1,3 +1,23 @@
+const https = require('https');
+
+const loadAPI = () => {
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+        let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        console.log(JSON.parse(data).explanation);
+    });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+})
+}
 const getMove = (enemyMove) => {
     var characters = 'ABCDEFGH';
     const rand1 = Math.floor(Math.random() * 7)+1
