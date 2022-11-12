79cf24d8b79969621fa26cdd89e1bcb02438f4d3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-12 08:22:44 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -1,3 +1,30 @@
+var http = require('http’);
+
+function httpGet(query, callback) {
+    var options = {
+        host: 'numbersapi.com',
+        path: '/' + encodeURIComponent(query),
+        method: 'GET',
+    };
+
+    var req = http.request(options, res => {
+        res.setEncoding('utf8');
+        var responseString = "";
+        
+        //accept incoming data asynchronously
+        res.on('data', chunk => {
+            responseString = responseString + chunk;
+        });
+        
+        //return the data when streaming is complete
+        res.on('end', () => {
+            console.log(responseString);
+            callback(responseString);
+        });
+
+    });
+    req.end();
+}
 
 const getMove = (enemyMove) => {
     var characters = 'ABCDEFGH';
