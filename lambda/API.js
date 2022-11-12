00b875f4c16de6c0dcf0e32b56761bfeb721f6d0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-12 12:57:18 +0000

    updated lambda/API.js.

---
+++ lambda/API.js
@@ -0,0 +1,19 @@
+const Fetch = require('node-fetch');
+
+async function getUser() {
+    try {
+      const response = await Fetch.fetch('https://www.randomnumberapi.com/api/v1.0/random?min=0&max=77&count=2');
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      
+      console.log(result)
+
+      return (result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
