e63e4341b552a9798f99e0736f7fa0283bf83a01
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-12 07:49:43 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -38,7 +38,7 @@ const chess_moveIntentHandler = {
         var enemyMove = `\nenemy moved ${number3}`;
         
         return handlerInput.responseBuilder
-            .speak(speakOutput + pause + enemyMove)
+            .speak(speakOutput + test.pause(number,number2) + enemyMove)
             .reprompt(speakOutput)
             .getResponse();
     }
